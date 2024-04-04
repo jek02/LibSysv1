a7ba81249f949f472c6d6af8d7fd57e77d95caf2
jek02 <102463674+jek02@users.noreply.github.com>
2024-04-04 16:27:24 +0800

    Linked na kung sino naka login

--- login.php
+++ login.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>LibSys Login</title>
     <link rel="stylesheet" href="css/styles.css"> <!-- Link to external CSS file -->
 </head>
 <body>
