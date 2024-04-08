88bd8d24d3578cc01d4b5c10ab6c6fc5918902b1
Notee01 <101891662+Notee01@users.noreply.github.com>
2024-04-08 16:38:38 +0800

    backup

--- employee/book_list.php
+++ employee/book_list.php
@@ -117,14 +117,14 @@ if ($user_result && $user_result->num_rows > 0) {
     echo "<table class='table table-bordered table-hover'>";
     echo "<thead class='thead-light'>";
     echo "<tr>";
-    echo "<th style='width: 5%; font-weight: bold; text-align: center;'>ID</th>";
-    echo "<th style='width: 25%; font-weight: bold; text-align: center;'>File Name</th>";
-    echo "<th style='width: 20%; font-weight: bold; text-align: center;'>Author</th>";
-    echo "<th style='width: 10%; font-weight: bold; text-align: center;'>Year</th>";
-    echo "<th style='width: 20%; font-weight: bold; text-align: center;'>Type of Publication</th>";
-    echo "<th style='width: 20%; font-weight: bold; text-align: center;'>Status</th>";
-    echo "<th style='width: 20%; font-weight: bold; text-align: center;'>Action</th>";
-    echo "<th style='width: 20%; font-weight: bold; text-align: center;'>Comments</th>";
+    echo "<th style='width: 5%; font-weight: bold; text-align: center;'class='text-center align-middle'>ID</th>";
+    echo "<th style='width: 25%; font-weight: bold; text-align: center;'class='text-center align-middle'>File Name</th>";
+    echo "<th style='width: 15%; font-weight: bold; text-align: center;'class='text-center align-middle'>Uploaded By</th>";
+    echo "<th style='width: 10%; font-weight: bold; text-align: center;'class='text-center align-middle'>Date Uploaded</th>";
+    echo "<th style='width: 15%; font-weight: bold; text-align: center;'class='text-center align-middle'>Type of Publication</th>";
+    echo "<th style='width: 10.5%; font-weight: bold; text-align: center;'class='text-center align-middle'>Status</th>";
+    echo "<th style='width: 20%; font-weight: bold; text-align: center;'class='text-center align-middle'>Action</th>";
+    echo "<th style='width: 20%; font-weight: bold; text-align: center;'class='text-center align-middle'>Comments</th>";
     echo "</tr>";
     echo "</thead>";
     echo "<tbody id='file-table-body'>";
@@ -136,7 +136,7 @@ if ($user_result && $user_result->num_rows > 0) {
         echo "<td class='text-center align-middle'><b>" . $row['author'] . "</b></td>";
         echo "<td class='text-center align-middle'><b>" . $row['year'] . "</b></td>";
         echo "<td class='text-center align-middle'><b>" . $row['type_of_publication'] . "</b></td>";
-        
+        echo "<td class='text-center align-middle'><b>" . $row['status'] . "</b></td>";
         echo "<td class='text-center align-middle'>";
         echo "<a href='download.php?id=" . $row['bid'] . "' class='btn btn-primary mr-2'>Download</a>";
         echo "<a href='view.php?id=" . $row['bid'] . "' class='btn btn-success'>View</a>";
